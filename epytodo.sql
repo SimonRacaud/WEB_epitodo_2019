16b208c4c3bd86d60ab27cfeb2fd433ece04461f
RACAUD Simon <simonracaud@gmail.com>
2020-03-18 18:55:23 +0100

    FEAT(epytodo database) : sql file of the basic database

--- epytodo.sql
+++ epytodo.sql
@@ -1,46 +1,46 @@
--- phpMyAdmin SQL Dump
--- version 4.9.4deb1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 18, 2020 at 06:01 PM
--- Server version: 10.3.22-MariaDB-1
--- PHP Version: 7.4.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `begin` datetime DEFAULT current_timestamp(),
+  `end` datetime DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'not started'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+INSERT INTO `task` (`task_id`, `title`, `begin`, `end`, `status`) VALUES
+(1, 'Test task', '2020-03-18 18:00:12', NULL, 'not started');
 
---
--- Database: `epytodo`
---
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `logged` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_has_task`
---
+INSERT INTO `user` (`user_id`, `username`, `password`, `logged`) VALUES
+(1, 'simon', 'simon', 0);
 
 CREATE TABLE `user_has_task` (
   `fk_user_id` int(11) NOT NULL,
   `fk_task_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user_has_task`
---
-
 INSERT INTO `user_has_task` (`fk_user_id`, `fk_task_id`) VALUES
 (1, 1);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_id`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `task`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
