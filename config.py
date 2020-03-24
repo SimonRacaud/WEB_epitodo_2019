40d2f7cfc2d6501270e878e18516fda6d0361abb
RACAUD Simon <simonracaud@gmail.com>
2020-03-24 11:40:49 +0100

    Feat(config.py) : set session timeout at 5 minutes

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 # @Date:   2020-03-17T15:19:34+01:00
 # @Project: WEB_epytodo_2019
 # @Last modified by:   simon
-# @Last modified time: 2020-03-20T14:05:12+01:00
+# @Last modified time: 2020-03-24T11:39:01+01:00
+
+from datetime import timedelta
 
 DATABASE_NAME = "epytodo"
 
@@ -15,3 +17,5 @@ DATABASE_USER = "root"
 DATABASE_PASS = "root"
 
 SECRET_KEY = 'I_love_cookies'
+
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=8)
