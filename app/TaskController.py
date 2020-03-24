04bd3621ad1fdbdf3edd27e9ef04100107ec5905
RACAUD Simon <simonracaud@gmail.com>
2020-03-24 11:54:22 +0100

    Feat(TaskController::__check_status_value) : check given status value (add and update task)

--- app/TaskController.py
+++ app/TaskController.py
@@ -2,7 +2,7 @@
 # @Date:   2020-03-17T15:19:40+01:00
 # @Project: PROJECT_NAME
 # @Last modified by:   simon
-# @Last modified time: 2020-03-24T11:26:11+01:00
+# @Last modified time: 2020-03-24T11:52:07+01:00
 
 from flask import render_template
 from flask import jsonify
@@ -15,6 +15,31 @@ class TaskController:
 
     def __init__(self):
         raise Exception("Error: class not instanciable")
+
+    def __check_format_datetime(self, argv = dict()):
+        if 'begin' in argv and argv['begin'] != None and (argv['begin'] != ""):
+            argv['begin'] = self.convert_str_datetime(argv['begin'])
+            if argv['begin'] == None:
+                print("check format datetime : begin value error")
+                return None
+            argv['begin'] = str(argv['begin'])
+        elif ('begin' in argv) and (argv['begin'] == ""):
+            argv['begin'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if ('end' in argv) and (argv['end'] != None) and (argv['end'] != ""):
+            argv['end'] = self.convert_str_datetime(argv['end'])
+            if argv['end'] == None:
+                print("check format datetime : end value error")
+                return None
+            argv['end'] = str(argv['end'])
+        elif ('end' in argv) and (argv['end'] == ""):
+            argv['end'] = None
+        return True
+
+    def __check_status_value(self, status = str()):
+        valid_values = ['not started', 'in progress', 'done']
+        if status in valid_values:
+            return True
+        return None
 
     def task_get_all(self):
         if self.is_logged():
@@ -58,30 +83,12 @@ class TaskController:
         else:
             return self.get_json_file_content("LOGGED_ERR.json")
 
-    def __check_format_datetime(self, argv = dict()):
-        if 'begin' in argv and argv['begin'] != None and (argv['begin'] != ""):
-            argv['begin'] = self.convert_str_datetime(argv['begin'])
-            if argv['begin'] == None:
-                print("check format datetime : begin value error")
-                return None
-            argv['begin'] = str(argv['begin'])
-        elif ('begin' in argv) and (argv['begin'] == ""):
-            argv['begin'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if ('end' in argv) and (argv['end'] != None) and (argv['end'] != ""):
-            argv['end'] = self.convert_str_datetime(argv['end'])
-            if argv['end'] == None:
-                print("check format datetime : end value error")
-                return None
-            argv['end'] = str(argv['end'])
-        elif ('end' in argv) and (argv['end'] == ""):
-            argv['end'] = None
-        return True
-
     def task_update_with_id(self, id, argv = dict()):
         if self.is_logged():
             id = self.get_int_id(id)
-            ret = self.__check_format_datetime(argv)
-            if id == None or len(argv) == 0 or ret == None:
+            ret_time = self.__check_format_datetime(argv)
+            ret_stat = self.__check_status_value(argv['status'])
+            if id == None or len(argv) == 0 or ret_stat == None or ret_time == None:
                 print("task_update_with_id : ERR argument value")
                 return self.get_json_file_content("INTERNAL_ERR.json")
             username = self.session.get_username()
@@ -96,8 +103,9 @@ class TaskController:
 
     def task_set_new(self, argv = dict()):
         if self.is_logged():
-            ret = self.__check_format_datetime(argv)
-            if len(argv) == 0 or ret == None:
+            ret_time = self.__check_format_datetime(argv)
+            ret_stat = self.__check_status_value(argv['status'])
+            if len(argv) == 0 or ret_time == None or ret_stat == None:
                 print("task_set_new : ERR argument value")
                 return self.get_json_file_content("INTERNAL_ERR.json")
             username = self.session.get_username()
