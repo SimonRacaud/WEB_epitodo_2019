7442fe3582659f84e14041afd03be1adcc102c7d
RACAUD Simon <simonracaud@gmail.com>
2020-03-24 11:27:42 +0100

    Fix(DataBase) : disable socket : connect()

--- app/DataBase.py
+++ app/DataBase.py
@@ -2,7 +2,7 @@
 # @Date:   2020-03-18T18:02:49+01:00
 # @Project: WEB_epytodo_2019
 # @Last modified by:   simon
-# @Last modified time: 2020-03-21T15:28:57+01:00
+# @Last modified time: 2020-03-24T10:37:12+01:00
 
 import pymysql as sql
 
@@ -47,7 +47,7 @@ class DataBase:
         try:
             self.database = sql.connect(
                 host        = app.config["DATABASE_HOST"],
-                unix_socket = app.config["DATABASE_SOCK"],
+                #unix_socket = app.config["DATABASE_SOCK"],
                 user        = app.config["DATABASE_USER"],
                 passwd      = app.config["DATABASE_PASS"],
                 db          = app.config["DATABASE_NAME"],
@@ -61,7 +61,7 @@ class DataBase:
     def disconnect(self):
         if (not hasattr(self, 'database')) or (self.database == None):
             return False;
-        self.database.close()
+        #self.database.close()
         return True
 
     def __del__(self):
